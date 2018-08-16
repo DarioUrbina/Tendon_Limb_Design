ec45bc74241f4a42facde623937b87a4c10f7753
Darío Urbina <urbiname@usc.edu>
2018-08-16 15:32:37 -0700

    Code reunderstood and applicable to leg

--- Tendon_Limb_Design/Code/Testing_LimbFigure.m
+++ Tendon_Limb_Design/Code/Testing_LimbFigure.m
@@ -1,4 +1,5 @@
 figure                                                 %Showguing force vectors
 
 %LimbFigure(q1_U, q2_U, l1, l2, leftXLimit, rightXLimit, upperYLimit, lowerYLimit)
-LimbFigure(1, 2, 10, 3, -10,25,-3,30)
+
+LimbFigure(30, 10, 1, 3, -5,5,-5,5)
