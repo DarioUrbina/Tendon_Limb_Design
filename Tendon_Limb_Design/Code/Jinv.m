70f1d5f4f12fe60f43148debecf359bc6b874d29
Darío Urbina <urbiname@usc.edu>
2018-08-16 13:02:31 -0700

    Adding First Version of the Code
    Intial Version, it still doesn't show the leg case

---
+++ Tendon_Limb_Design/Code/Jinv.m
@@ -0,0 +1,34 @@
+function J = Jinv(q_1, q_2, l1, l2)
+
+syms q1 q2
+
+A1 = [ cos(q1) -sin(q1) 0 0;
+        sin(q1) cos(q1) 0 0;
+        0       0       1   0;
+        0       0       0   1 ];
+    
+A2 = [ cos(q2) -sin(q2) 0 0;
+        sin(q2) cos(q2) 0 -l1;
+        0       0       1   0;
+        0       0       0   1 ];
+
+A3 = [ 1 0 0 0 ;
+        0 1 0 -l2;
+        0 0 1 0 ;
+        0 0 0 1 ] ;
+
+T0_1 = A1;
+T0_2 = A1 * A2;
+T0_3 = T0_2 * A3;
+
+r = T0_3;
+g = T0_3(1:2,4);
+J = jacobian(g,[q1 q2]);
+
+% q1 = degtorad(q_1); q2 = degtorad(q_2);
+q1 = q_1; q2 = q_2;
+
+J_sim = double(subs(simplify(J)));
+J = inv(J_sim);
+end
+
---
+++ Tendon_Limb_Design/Code/LimbFigure.m
@@ -0,0 +1,22 @@
+function D = LimbFigure(q1_U, q2_U, l1, l2)
+
+q1_F_Uplimit = q1_U;
+q2_F_Uplimit=q2_U;
+
+
+figure
+    x(1) = 0;
+    y(1) = 0;
+    x(2) = x(1) + l1 * cos((q1_F_Uplimit));
+    y(2) = y(1) + l1 * sin((q1_F_Uplimit));
+    x(3) = x(2) + l2 * cos((q2_F_Uplimit+q1_F_Uplimit)); %Here the angles need to be added in order to get q2 meassured from the first segment
+    y(3) = y(2) + l2 * sin((q2_F_Uplimit+q1_F_Uplimit));
+
+    plot(x(:),y(:));
+%     xlim([-15 15]);
+    title(['Posture with  q1:  ' num2str(rad2deg(q1_F_Uplimit))  'and    q2:  ' num2str(rad2deg(q2_F_Uplimit)) '  (deg) '])
+    xlabel('cm')
+    ylabel('cm')
+
+    hold on
+end
