f1c4fadd431d7c6d772c8915f7baae07eee03738
Your Name here <urbiname@usc.edu>
2018-08-21 11:27:39 -0700

    Cleaning code

--- Tendon_Limb_Design/Code/Tendon_Limb_Design.m
+++ Tendon_Limb_Design/Code/Tendon_Limb_Design.m
@@ -1,15 +1,14 @@
 function Tendon_Limb_Design(q)
 
 sizzze=size(q,1)
-% clear all
-% close all
 
-motorForce=49*.85; 	%__tendon tensions that the Faulhaber23.. motors can generate, 49 is in Stall Torque, we can't consider that value
+motorForce=49*.85; 	%[N]__tendon tensions that the Faulhaber23.. motors can generate, 49 N is in Stall Torque, we can't consider that value
 F01 = motorForce; F02 = motorForce; F03 = motorForce;  %Force values 
 
-r1 = 23; r2 = 23;                          % cm
+r1 = .23; r2 = .23;                          % m
+% r1 = 1; r2 = 1;
  
-l1 = 8; l2 = 7.84;                           % cm
+l1 = .08; l2 = .078;                           % m
 
 F0 = diag([F01; F02; F03]);                 %Diagnal matrix for Fmax           
 
@@ -19,16 +18,16 @@ F0 = diag([F01; F02; F03]);                 %Diagnal matrix for Fmax
 R_Front = [-r1 -r1 +r1 ;                   % Moment arm matrices
             r2  -r2 0 ];
 
-
         
 % q=[30 30;                       % Angle of the joints (for all the required postures)
 %     30 20
 % ];
         
-% size(q(1))
 % figure
 for i=1:sizzze
-        disp=i*100                       
+        disp=i*100 
+%         disp=0 ;                      
+
         J_inv_Front = Jinv(deg2rad(q(i,1)), deg2rad(q(i,2)), l1, l2);        %Inverse Jacobian
 
         % H_front =  R_Front * F0;                         %For torque space plots.
@@ -52,8 +51,8 @@ for i=1:sizzze
             1 1 0
             1 1 1];
     
-        vertex=zeros(16,1); 
-        vertey=zeros(16,1);  
+        vertex=zeros(8,1); 
+        vertey=zeros(8,1);  
 
 
                                                           %Showing force vectors
